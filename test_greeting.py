350dbc2bc3807eb8cb0a1cb2a2b2aea814ccd831
Paa Kofi Nyarkoh <paakofinyarkoh52@gmail.com>
2026-05-14 23:17:08 +0200

    my test file for greetings.py

---
+++ test_greeting.py
@@ -0,0 +1,9 @@
+import unittest
+from greetings import greet
+class TestGreet(unittest.TestCase):
+   def test_greet(self):
+    self.assertEqual(greet("Alice"), "Hello, Alice")
+    self.assertEqual(greet(""), "Hello, Stranger")
+    self.assertEqual(greet(None), "Hello, Stranger")
+if __name__ == "__main__":
+  unittest.main()
